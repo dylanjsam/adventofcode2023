549636d3b6e5fcd0b47845e11fa73b89ee11f707
dylanjsam <dylanjsam2000@gmail.com>
2023-12-06 14:14:09 +0100

    complete day 4

--- day4.py
+++ day4.py
@@ -1,5 +1,4 @@
-f = open('day4.txt', 'r')
-
+file = open('day4.txt', 'r')
 def part1(f):
     S = 0
     for line in f:
@@ -26,38 +25,43 @@ def part1(f):
         S += a
     return S
 #print(part1(f))
-def part2(f):
-    linenum = 1
-    S = 0
-    length = 0
-    dict = {}
-    for line in f:
-        length += 1
-        dict[str(length)] = int(1)
-    
-    for line in f:
-        sep = line.split("|")
-        z = 0
-        for side in sep:
-            if z == 0:
-                left = []
-                sept = side.split()
-                for i in range(2, len(sept)):
-                    left.append(sept[i])
-            else:
-                right = []
-                sept = side.split()
-                for i in range(0, len(sept)):
-                    right.append(sept[i])
 
-        count = 0
-        for number in left:
-            if number in right:
-                count += 1
-            print(count)
-        index = 1
-        for amount in range(0, count):
-            dict.update({str(index+linenum): int(dict[str(index+linenum)]) + 1})
-            index += 1
-        linenum += 1
-print(part2(f))
+
+linenum = 0
+S = 0
+length = 0
+dict = []
+for line in file:
+    length += 1
+    dict.append(1)
+
+file.seek(0)
+for line in file:
+    sep = line.split("|")
+    z = 0
+    for side in sep:
+        if z == 0:
+            left = []
+            sept = side.split()
+            for i in range(2, len(sept)):
+                left.append(sept[i])
+        else:
+            right = []
+            sept = side.split()
+            for i in range(0, len(sept)):
+                right.append(sept[i])
+        z += 1
+    count = 0
+    for number in right:
+        if number in left:
+            count += 1
+    index = 1
+    for amount in range(0, count):
+        dict[index+linenum] = dict[index+linenum] + dict[linenum] 
+        index += 1
+    linenum += 1
+S = 0
+for i in dict:
+    S += i
+print(S)
+file.close()
