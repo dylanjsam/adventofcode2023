492a663d895b1d9b6fb6998c50ccf1e87c4670d3
dylanjsam <dylanjsam2000@gmail.com>
2023-12-06 12:37:09 +0100

    day4

--- day4.py
+++ day4.py
@@ -24,5 +24,40 @@ def part1(f):
             elif number in right:
                 a = 2*a
         S += a
-    print(S)
-print(part1(f))
+    return S
+#print(part1(f))
+def part2(f):
+    linenum = 1
+    S = 0
+    length = 0
+    dict = {}
+    for line in f:
+        length += 1
+        dict[str(length)] = int(1)
+    
+    for line in f:
+        sep = line.split("|")
+        z = 0
+        for side in sep:
+            if z == 0:
+                left = []
+                sept = side.split()
+                for i in range(2, len(sept)):
+                    left.append(sept[i])
+            else:
+                right = []
+                sept = side.split()
+                for i in range(0, len(sept)):
+                    right.append(sept[i])
+
+        count = 0
+        for number in left:
+            if number in right:
+                count += 1
+            print(count)
+        index = 1
+        for amount in range(0, count):
+            dict.update({str(index+linenum): int(dict[str(index+linenum)]) + 1})
+            index += 1
+        linenum += 1
+print(part2(f))
