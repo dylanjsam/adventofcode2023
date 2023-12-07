ab8ca230fea4c6ef90f8c27da6034c68dffc6532
dylanjsam <dylanjsam2000@gmail.com>
2023-12-07 10:31:14 +0100

    okay

--- day5.py
+++ day5.py
@@ -2,11 +2,11 @@ f = open('day5.txt', 'r')
 def findloc(seed, maps):
         print(seed)
 
-def conv(file):
-    linenum = 0
-    file.readline()
-    file.readline()
-    for line in file:
-        p = line.split()
-        
-print(conv(f))
+def inputoutput(input, map):
+    for line in map:
+          output = input
+          if input >= line[1] and input <= (line[1]+line[2]):
+                output = line[0] + input - line[1]
+    return output
+
+print(inputoutput(500, [[50, 450, 100], [30, 500, 50]]))
