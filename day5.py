794db7b2b34558f4941e5b91c711cf21a612e7fe
dylanjsam <dylanjsam2000@gmail.com>
2023-12-07 10:21:08 +0100

    added day 5

---
+++ day5.py
@@ -0,0 +1,12 @@
+f = open('day5.txt', 'r')
+def findloc(seed, maps):
+        print(seed)
+
+def conv(file):
+    linenum = 0
+    file.readline()
+    file.readline()
+    for line in file:
+        p = line.split()
+        
+print(conv(f))
